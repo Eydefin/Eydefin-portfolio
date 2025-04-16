b8157371c903e2f6b32c5f7be49e6f1ddbe3ebdb
Eydefin <eydefin@outlook.com>
2025-04-16 08:57:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './Home.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+console.log("Portfolio loaded successfully!");
